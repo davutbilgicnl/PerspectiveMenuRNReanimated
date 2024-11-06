db1a34a54c995093e78370453c7c7ad4a202b5ae
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-11-06 15:42:03 +0100

    Refactor App component to use SafeAreaView and add PerspectiveMenu component

---
+++ components/PerspectiveMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const PerspectiveMenu = () => {
+  return (
+    <View>
+      <Text>Perspective Menu</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default PerspectiveMenu;
